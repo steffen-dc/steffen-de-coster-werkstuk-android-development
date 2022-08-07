cb83b23c6fedf02ea8da78f9082db9f2fe4db2d2
steffen-dc <steffen.de.coster@student.ehb.be>
2022-08-07 11:31:33 +0200

    add underscore to search games fragment filename

--- app/src/main/java/com/example/cheapfreegames/ui/games/SearchGamesFragment.kt
+++ app/src/main/java/com/example/cheapfreegames/ui/games/SearchGamesFragment.kt
@@ -6,18 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.cheapfreegames.databinding.FragmentSearchgamesBinding
+import com.example.cheapfreegames.databinding.FragmentSearchGamesBinding
 
 class SearchGamesFragment : Fragment() {
 
     private val viewModel: SearchGamesViewModel by viewModels()
 
-    // Binding object instance corresponding to the fragment_searchgames.xml layout
-    private var _binding: FragmentSearchgamesBinding? = null
+    // Binding object instance corresponding to the fragment_search_games.xml layout
+    private var _binding: FragmentSearchGamesBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentSearchgamesBinding.inflate(inflater) // string
+        _binding = FragmentSearchGamesBinding.inflate(inflater) // string
         // val binding = GridViewItemBinding.inflate(inflater) // image
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
